efe7d22e2c9a707e32ff855b3e6c0c966cdd5e32
Lars <lars-vb@hotmail.com>
2018-02-04 16:23:58 +0100

    Lecture 1 finished, Added Test envo in C#

---
+++ Huiswerk/TestEnvo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEnvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            greatestCommonDivisor(20, 15);
+        }
+
+        static void greatestCommonDivisor(int x, int y)
+        {
+
+            Math.DivRem(x, y, out int rem);
+            int wat = x % y;
+
+            int BIG;
+            int SMALL;
+
+            if(x > y)
+            {
+                BIG = x;
+                SMALL = y;
+            } else
+            {
+                BIG = y;
+                SMALL = x;
+            }
+        }
+    }
+}
